ff0b942f9f1b2e0f7d9f5e562fe54c66045be628
Alex Xenon <b3haply@gmail.com>
2024-09-12 09:10:29 +0300

    Added base interface TextExtractor

---
+++ src/main/java/lab1/TextExtractor.java
@@ -0,0 +1,23 @@
+package main.java.lab1;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author Alex
+ * */
+public interface TextExtractor {
+
+    /**
+     * The pattern that should be overridden by each implementation
+     * */
+    Pattern pattern = null;
+
+    /**
+     * Extracts text from a given input string using the specified pattern.
+     *
+     * @param text The input string to be extracted from.
+     * @return The extracted text, or empty string if no match is found.
+     */
+    String extract(String text);
+
+}
