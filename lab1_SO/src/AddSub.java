dc70b5665f48b06cc37d0f95c239b900b08aa1a6
MaxVoicu7 <mxmvoicu@gmail.com>
2024-09-15 18:33:14 +0300

    Revert merge
    This reverts commit 65775e8abdb398ee6c68761625a6bf02441ad689.

--- lab1_SO/src/AddSub.java
+++
@@ -1,9 +0,0 @@
-public class AddSub {
-    public double add(double a, double b) {
-        return a + b;
-    }
-
-    public double sub(double a, double b) {
-        return a - b;
-    }
-}
--- lab1_SO/src/PowRoot.java
+++
@@ -1,5 +0,0 @@
-public class PowRoot {
-    public double power(double a, double b) {
-        return Math.pow(a, b);
-    }
-}
