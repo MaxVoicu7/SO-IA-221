a17d58558a7fdeece802550a354e2cd8267dbe76
PopusoiCatalin <catalinpopusoi73@gmail.com>
2024-09-18 14:30:20 +0300

    Modificarea clasei Dice

--- Dice.java
+++ Dice.java
@@ -1,3 +1,19 @@
-public class Dice {
+import java.util.Random;
 
+class Dice {
+    private int numFaces;
+    private Random random;
+
+    public Dice(int numFaces) {
+        this.numFaces = numFaces;
+        this.random = new Random();
+    }
+
+    public int roll() {
+        return random.nextInt(numFaces) + 1;
+    }
+
+    public int getNumFaces() {
+        return numFaces;
+    }
 }
