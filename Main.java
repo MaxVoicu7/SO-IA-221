27e355f38028bba6c9e30a29cc74d99ccd25a55e
Chisnenco Dinu <dinuchisnenco@gmail.com>
2024-09-18 14:53:18 +0300

    Modificarea clasei Main

--- Main.java
+++ Main.java
@@ -1,3 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+        DiceGame game = new DiceGame();
 
+        game.addDice(new Dice(6));
+        game.addDice(new Dice(12));
+
+        game.play();
+
+        System.out.println("Rezultatele aruncarilor: " + game.getResults());
+    }
 }
