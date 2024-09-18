02318e5fd4da147899ba4b9e855c89f417ffb5fa
Chisnenco Dinu <dinuchisnenco@gmail.com>
2024-09-18 14:44:32 +0300

    Modificarea clasei DiceGame

--- DiceGame.java
+++ DiceGame.java
@@ -1,2 +1,32 @@
-public class DiceGame {
+import java.util.ArrayList;
+import java.util.List;
+
+class DiceGame {
+    private List<Dice> diceList;
+    private List<Integer> results;
+
+    // Constructor
+    public DiceGame() {
+        diceList = new ArrayList<>();
+        results = new ArrayList<>();
+    }
+
+    public void addDice(Dice dice) {
+        diceList.add(dice);
+    }
+
+    public void play() {
+        System.out.println("Arunc zarurile...");
+        results.clear();
+
+        for (Dice dice : diceList) {
+            int result = dice.roll();
+            results.add(result);
+            System.out.println("Zar cu " + dice.getNumFaces() + " fețe a dat: " + result);
+        }
+    }
+
+    public List<Integer> getResults() {
+        return results;
+    }
 }
