96db79a6d245ff142b9ef598ba085ea950d2a1ac
anastasia neceaeva <anastasianeceaeva@MacBook-Pro-anastasia.local>
2024-09-19 10:20:07 +0300

    Am adaugat partea mea cu conversia valutelor ron si mdl la partea principala

--- lab1SO/src/Main.java
+++ lab1SO/src/Main.java
@@ -6,9 +6,17 @@ public class Main {
         double eurToUsdRate = 1.1;  // 1 EUR = 1.1 USD
         double usdToEurRate = 0.91; // 1 USD = 0.91 EUR
 
+        //Adaugarea cursului MDL,RON
+        double ronToMdlRate = 3.8;  // 1 RON = 3.8 MDL
+        double mdlToRonRate = 0.26; // 1 MDL = 0.26 RON
+
 
         EuroConverter euroConverter = new EuroConverter(eurToUsdRate);
         UsdConverter usdConverter = new UsdConverter(usdToEurRate);
+
+        //Adaugarea conversiei
+        RonConverter ronConverter = new RonConverter(ronToMdlRate);
+        MdlConverter mdlConverter = new MdlConverter(mdlToRonRate);
 
         Scanner scanner = new Scanner(System.in);
         String continuaConversia;
@@ -18,8 +26,10 @@ public class Main {
             System.out.println("Alegeți tipul de conversie:");
             System.out.println("1: EUR -> USD");
             System.out.println("2: USD -> EUR");
+            System.out.println("3: RON -> MDL");
+            System.out.println("4: MDL -> RON");
 
-            System.out.print("Introduceți opțiunea (1 sau 2): ");
+            System.out.print("Introduceți opțiunea (1, 2, 3, 4): ");
             int option = scanner.nextInt();
 
             switch (option) {
@@ -39,8 +49,22 @@ public class Main {
                     System.out.println(amountInUsd + " USD = " + amountInEur + " EUR");
                     break;
 
+                case 3:
+                    System.out.print("Introduceți suma în RON pentru a fi convertită în MDL: ");
+                    double amountInRon = scanner.nextDouble();
+                    double amountInMdl = ronConverter.convertTo(amountInRon);
+                    System.out.println(amountInRon + " RON = " + amountInMdl + " MDL");
+                    break;
+
+                case 4:
+                    System.out.print("Introduceți suma în MDL pentru a fi convertită în RON: ");
+                    amountInMdl = scanner.nextDouble();
+                    amountInRon = mdlConverter.convertTo(amountInMdl);
+                    System.out.println(amountInMdl + " MDL = " + amountInRon + " RON");
+                    break;
+
                 default:
-                    System.out.println("Opțiune invalidă! Te rog să alegi 1 sau 2.");
+                    System.out.println("Opțiune invalidă! Te rog să alegi 1, 2, 3, 4.");
                     break;
             }
 
