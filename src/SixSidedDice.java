34e4254e3efd157b6ec1e331742f5a8e3fb34c8d
PopusoiCatalin <catalinpopusoi73@gmail.com>
2024-09-22 15:26:39 +0300

    Modificarea clasei SixSidedDice

--- src/SixSidedDice.java
+++ src/SixSidedDice.java
@@ -1,2 +1,21 @@
-public class SixSidedDice {
+class SixSidedDice extends Dice {
+    public SixSidedDice() {
+        super(6);  // Setează 6 fețe
+    }
+
+    @Override
+    public String getDescription() {
+        return "Zar cu 6 fețe";
+    }
+}
+
+class TwelveSidedDice extends Dice {
+    public TwelveSidedDice() {
+        super(12);  // Setează 12 fețe
+    }
+
+    @Override
+    public String getDescription() {
+        return "Zar cu 12 fețe";
+    }
 }
