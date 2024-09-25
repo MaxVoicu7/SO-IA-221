0de2eb7fd52a7a9ea2ce674f523577df36547807
Chisnenco Dinu <dinuchisnenco@gmail.com>
2024-09-25 20:44:18 +0300

    țModificarea clasei Main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,40 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         DiceGame game = new DiceGame();
+        Scanner scanner = new Scanner(System.in);
+        int option;
 
-        game.addDice(new Dice(6));
-        game.addDice(new Dice(12));
+        do {
+            System.out.println("\nMeniu:");
+            System.out.println("1. Adaugă zar cu 6 fețe");
+            System.out.println("2. Adaugă zar cu 12 fețe");
+            System.out.println("3. Aruncă zarurile");
+            System.out.println("4. Ieșire");
+            System.out.print("Alege o opțiune: ");
+            option = scanner.nextInt();
 
-        game.play();
+            switch (option) {
+                case 1:
+                    game.addDice(new SixSidedDice());
+                    System.out.println("Ai adăugat un zar cu 6 fețe.");
+                    break;
+                case 2:
+                    game.addDice(new TwelveSidedDice());
+                    System.out.println("Ai adăugat un zar cu 12 fețe.");
+                    break;
+                case 3:
+                    game.play();
+                    System.out.println("Rezultatele aruncărilor: " + game.getResults());
+                    break;
+                case 4:
+                    System.out.println("Ieșire...");
+                    break;
+                default:
+                    System.out.println("Opțiune invalidă. Încearcă din nou.");
+            }
+        } while (option != 4);
 
-        System.out.println("Rezultatele aruncarilor: " + game.getResults());
+        scanner.close();
     }
 }
