e138c8f3e106dfcbb8fc80bb83f4d6a33981cde0
Adrian Armanu <adrian.armanu@mail.ru>
2024-09-25 09:48:50 +0300

    Comentarii PowRoot si MultDiv

--- lab1_SO/src/MultDiv.java
+++ lab1_SO/src/MultDiv.java
@@ -1,13 +1,18 @@
+// Clasa MultDiv contine metode pentru inmultire si impartire a doua numere
 public class MultDiv {
 
+    // Metoda mult returneaza produsul dintre doua numere de tip double
     public double mult(double a, double b){
-        return a * b;
+        return a * b; // inmulteste a si b
     }
 
+    // Metoda div returneaza rezultatul impartirii dintre doua numere de tip double
     public double div(double a, double b){
+        // Verifica daca b este diferit de zero pentru a evita impartirea la zero
         if(b != 0){
-            return a / b;
+            return a / b; // daca b nu este zero, returneaza rezultatul impartirii
         } else {
+            // In caz contrar, afiseaza un mesaj de eroare si returneaza 0
             System.out.println("Impartirea la zero nu e permisa!");
             return 0;
         }
--- lab1_SO/src/PowRoot.java
+++ lab1_SO/src/PowRoot.java
@@ -1,9 +1,13 @@
+// Clasa PowRoot contine metode pentru ridicarea la putere si extragerea radacinii unui numar
 public class PowRoot {
+
+    // Metoda power returneaza rezultatul ridicarii la putere a numarului a la exponentul b
     public double power(double a, double b) {
-        return Math.pow(a, b);
+        return Math.pow(a, b); // utilizeaza metoda Math.pow pentru ridicarea lui a la puterea b
     }
 
+    // Metoda root returneaza rezultatul extragerii radacinii n din numarul a
     public double root(double a, double n) {
-        return Math.pow(a, 1.0 / n);
+        return Math.pow(a, 1.0 / n); // foloseste Math.pow pentru a calcula radacina n-a din a
     }
 }
