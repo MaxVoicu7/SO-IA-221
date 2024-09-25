b7fd469cd444118a4dffd96f55b83e8dfa898692
Istru2003 <istru03@gmail.com>
2024-09-25 09:46:56 +0300

    Comentarile la AddSub

--- lab1_SO/src/AddSub.java
+++ lab1_SO/src/AddSub.java
@@ -1,9 +1,12 @@
+// Clasa AddSub contine metode pentru adunarea si scaderea a doua numere
 public class AddSub {
+    // Metodă pentru adunarea a două numere de tip double
     public double add(double a, double b) {
-        return a + b;
+        return a + b; // Returnăm suma celor două numere
     }
 
+    // Metodă pentru scăderea a două numere de tip double
     public double sub(double a, double b) {
-        return a - b;
+        return a - b; // Returnăm diferența dintre cele două numere
     }
 }
