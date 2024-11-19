af7ec933b6ca6b8289801cbd57690a04c975ab76
MaxVoicu7 <mxmvoicu@gmail.com>
2024-11-19 22:02:20 +0200

    Improved code

--- lab5/Lab.java
+++ lab5/Lab.java
@@ -1,54 +1,61 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.concurrent.TimeUnit;
 
 public class Lab {
-	public static void main(String args[]) {
-		try {
-			FileWriter writer = new FileWriter("/home/voicu/Desktop/message.txt" , true);
-			
-			LocalDateTime now = LocalDateTime.now();
-			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-			
-			System.out.println("Choose operation: ");
-			System.out.println("\t1. Shutdown");
-			System.out.println("\t2. Reboot");
-			
-			java.util.Scanner scanner = new java.util.Scanner(System.in);
-			int choice = scanner.nextInt();
-		
-			
-			ProcessBuilder processBuilder;
-			String logMessage;
-			
-			if (choice == 1) {
-				processBuilder = new ProcessBuilder("shutdown", "-h", "+1");
-				logMessage = "Shutdown at " + now.format(formatter) + "\n";
-				
-				writer.write(logMessage);
-				writer.close();
+    public static void main(String[] args) {
+        String desktopPath;
+        String os = System.getProperty("os.name").toLowerCase();
+        
+        try {
 
-				processBuilder.inheritIO();
-				Process process = processBuilder.start();
-				
-				System.out.println("operation in 1 minute");
-			} else if (choice == 2) {
-				processBuilder = new ProcessBuilder("shutdown", "-r", "+1");
-				logMessage = "Reboot at " + now.format(formatter) + "\n";
-				
-				writer.write(logMessage);
-				writer.close();
+                desktopPath = System.getProperty("user.home") + 
+                    File.separator + "Desktop" + 
+                    File.separator + "so.txt";
+                
+                File desktop = new File(System.getProperty("user.home") + File.separator + "Desktop");
+                if (!desktop.exists() || !desktop.isDirectory()) {
+                    throw new IOException("Desktop not found!");
+                }
+            
 
-				processBuilder.inheritIO();
-				Process process = processBuilder.start();
-				
-				System.out.println("operation in 1 minute");
-			}
-			
-		} catch (IOException e) {
-			System.err.println(e.getMessage());
-		}
-	}
+            FileWriter writer = new FileWriter(desktopPath, true);
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            
+            System.out.println("Choose operation: ");
+            System.out.println("1. Restart");
+            System.out.println("2. Shutdown");
+            
+            java.util.Scanner scanner = new java.util.Scanner(System.in);
+            int choice = scanner.nextInt();
+            
+            ProcessBuilder processBuilder;
+            String logMessage;
+
+                if (choice == 1) {
+                    processBuilder = new ProcessBuilder("shutdown", "-r", "+1");
+                    logMessage = "restart: " + now.format(formatter) + "\n";
+                } else {
+                    processBuilder = new ProcessBuilder("shutdown", "-h", "+1");
+                    logMessage = "shutdown: " + now.format(formatter) + "\n";
+                }
+            
+            
+            writer.write(logMessage);
+            writer.close();
+            
+            processBuilder.inheritIO();
+            Process process = processBuilder.start();
+            
+            System.out.println("wait 1 minute...");
+            
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
+
